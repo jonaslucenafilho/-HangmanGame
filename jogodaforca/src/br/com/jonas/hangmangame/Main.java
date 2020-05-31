4f017f0c03496d17dc1d17e0133d2cba7cf46e64
Jonas Lucena <jonaslucena@outlook.com>
2020-05-31 15:25:35 -0300

    initial implementation of Word class and tested

--- jogodaforca/src/br/com/jonas/hangmangame/Main.java
+++ jogodaforca/src/br/com/jonas/hangmangame/Main.java
@@ -1,8 +1,11 @@
 package br.com.jonas.hangmangame;
 
+import br.com.jonas.hangmangame.game.Game;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Game game = new Game();
+		game.start();
 	}
 }
--- jogodaforca/src/br/com/jonas/hangmangame/game/Game.java
+++ jogodaforca/src/br/com/jonas/hangmangame/game/Game.java
@@ -1,5 +1,17 @@
 package br.com.jonas.hangmangame.game;
 
+import br.com.jonas.hangmangame.core.Word;
+
 public class Game {
 
+	public void start() {
+		
+		Word word = new Word("casa");
+		word.hasChar('a');
+		word.hasChar('c');
+		word.hasChar('s');
+		System.out.println(word.discovered());
+		
+		System.out.println(word);
+	}
 }
